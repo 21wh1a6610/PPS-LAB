cfe6430d0cdfb17ae1d2be9fa132e729008afd41
21wh1a6610 <96616282+21wh1a6610@users.noreply.github.com>
2022-01-04 16:15:05 +0530

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int num1=4;
+int main()
+{
+	int num2=7;
+	printf("num2 is %d",num2);
+	printf("num1 is %d",num1);
+        fun1();
+        printf("num1 is %d",num1);
+}
+int fun1()
+{
+	int num1=5;
+	printf("num1 is %d",num1);
+}
+
