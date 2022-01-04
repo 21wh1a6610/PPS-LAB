ea07d55fd5f88978c342aa58b0ce4400c84d8a98
21wh1a6610 <96616282+21wh1a6610@users.noreply.github.com>
2022-01-04 16:10:36 +0530

    Add files via upload

---
+++ test5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+float sum,count;
+int mean;
+printf("enter the value of sum and count");
+scanf("%f%f",&sum,&count);
+mean=sum/count;
+printf("the value of mean is:%d\n",mean);
+}
